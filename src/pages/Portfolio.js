6a194e3e862381f9db6f3d7e41dbfb1966d668ec
Melli-Alice <makeupmellialice@gmail.com>
2022-07-27 20:06:49 -0700

    quick fix for portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,24 +4,9 @@ import PageTemplate from '../components/PageTemplate';
 import ImageGallery from '../components/ImageGallery';
 
 export default function Portfolio() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    axios.get('https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,timestamp&access_token=IGQVJXbjBmODZAOZAG1ib3hlOXBMWmhjWTdFeUhoWDhFOEdaMmdLeUxGQm9jVzNrcE8wRWdLWFZAkYXd4ZA2dadmI0OW5sT0tnZAFBQa2ZARNUpuanJGWXRBcno0SUVHN1FaYU5jTDFMOUR5RzJUSmhQaUxjQwZDZD')
-      .then(res => {
-        setImages(res.data.data);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <PageTemplate title='Portfolio'>
-      {
-        isLoading ?
-          <div>Loading...</div> :
-          <ImageGallery images={images} />
-      }
+      <ImageGallery />
     </PageTemplate>
   );
 }
