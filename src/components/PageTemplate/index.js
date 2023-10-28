ccf0f4c7fa0ea41b1d01d208573bc1cd77459759
Melli-Alice <makeupmellialice@gmail.com>
2023-10-28 13:57:08 -0700

    page template redesigned

--- src/components/PageTemplate/index.js
+++ src/components/PageTemplate/index.js
@@ -1,24 +1,42 @@
 import MainNav from '../MainNav';
-import LogoText from '../LogoText';
-import Footer from '../Footer';
+import corner from '../../images/decorations/corner.png';
+import divider from '../../images/decorations/divider1.png';
+import divider2 from '../../images/decorations/divider2.png';
+import side from '../../images/decorations/side.png';
+import tag from '../../images/decorations/tag.png';
 import './PageTemplate.scss';
 
 export default function PageTemplate({ children, title }) {
   return (
-    <div className='page'>
-      <div className='app'>
-        <header className='app__header'>
-          <LogoText />
-        </header>
-        <div className='app__content'>
-          <MainNav />
-          <div className='app__content--container'>
-            <h2 className='app__content--title'>{title}</h2>
-            {children}
+    <div className='homepage'>
+      <div className='page-content'>
+        <div className='homepage__top'>
+          <div className='homepage__title'>
+            <img className='homepage__corner--left' src={corner} />
+            <h1 className='title-text'>Sarina Keys Tattoo</h1>
+            <img className='homepage__corner--right' src={corner} />
+          </div>
+          <div className='address'>Colby Ave Tattoo / 2712 Colby Ave, Everett, WA 98201 / (425) 658 2271</div>
+          <div className='homepage__divider-container'>
+            <img className='homepage__divider' src={divider} />
           </div>
         </div>
+        <div className='nav-container'>
+          <img className='nav-container__tag--left' src={tag} />
+          <MainNav />
+          <img className='nav-container__tag--right' src={tag} />
+        </div>
+        <div className='page-content__main'>
+          <img className='page-content__side' src={side} />
+          <div className='page-content__center'>
+            {children}
+          </div>
+          <img className='page-content__side' src={side} />
+        </div>
+        <div className='homepage__bottom'>
+          <img src={divider2} />
+        </div>
       </div>
-      <Footer />
     </div>
   );
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PageTemplate from '../components/PageTemplate';
-import './Main.scss';
+import profile from '../images/profile.jpeg';
+import border from '../images/decorations/transparentborder.png';
 
 export default function Main() {
   return (
-    <div className='homepage'>
-      <PageTemplate />
-    </div>
+    <PageTemplate>
+      <div className='profile-image-container'>
+        <img className='profile-image' src={profile} />
+        <img className='profile-image__border' src={border} />
+      </div>
+    </PageTemplate>
   );
 }
-
-
